7ccf15195591071376f7ca9352724a9988bf883e
Tim Dill <brodt7258@gmail.com>
2019-07-11 06:26:27 -0400

    add some addition data to initialState

--- droom/src/stateManagement/reducers/index.js
+++ droom/src/stateManagement/reducers/index.js
@@ -5,11 +5,25 @@ const initialState = {
     1: {
       id: 1,
       username: 'test',
-      password: '123'
+      password: '123',
+      type: 'job-seeker'
+    },
+    2: {
+      id: 2,
+      username: 'Initech',
+      password: 'password',
+      type: 'company'
     }
   },
   listings: {
-
+    1: {
+      id: 1,
+      ownderId: 2,
+      title: 'Synergy Analyst',
+      desc: 'You write memos.  No one reads them.',
+      skills: 'Literally anyone could do this',
+      salary: 'ramen'
+    }
   },
   matches: [],
   currentUser: null,
